a31ba62ca3aad1776ff1f33b3ddcf9689cac428d
Роман Г. Ахметов <r.ahmetov@sumteh.local>
2018-07-16 18:20:04 +0500

    crossroad_16_7_0

---
+++ Yandex/Webmaster/settings/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+      'APP_ID'=>'a9b88a6ce0cd4f05a9bebbec29808768',
+      'APP_PASS'=>'1a456f1df96843869b08280bfcf5727a',
+      'OAUTH_TOKEN'=>'AQAAAAAJPoHXAAUaL_wykqhhGURpn3oIAdBDAfI', //Авторизационный токен прилжения
+      'COUNTER_ID'=>'45275493'  //ID счётчика для yandex metrika
+];
+
+?>
